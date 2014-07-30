83b8c1411a4ccbbb27ebfbef79a2cbc5278aac78
Johan Bleuzen <johan@bleuzen.fr>
2014-07-30 06:34:36 +0200

    Fix: Merge was not valid

--- www/index.php
+++ www/index.php
@@ -2,61 +2,57 @@
 
 include '../bootstrap.php';
 
+use Core\Application as Application;
 
-HelperConfig::loadConfig();
+use App\Helpers\Picturo as Picturo;
+use Core\Helper as Helper;
 
+$app = new Application();
 
 // Routing
 $router = new \Bramus\Router\Router();
 
-$router->before('GET|POST', '/.*', function() {
+$router->before('GET|POST', '/.*', function() use ($app) {
   global $config;
 
   if ($config['private'] == true && !isset($_SESSION['username']) && ! preg_match("@/login@", $_SERVER["REQUEST_URI"])) {
-    HelperConfig::redirect('/login');
+    $app->redirect('/login');
   }
 });
 
-$router->get('/login', function() {
-  $controller = new ControllerUser();
-  $controller->get_login();
+$router->get('/login', function() use ($app) {
+  $controller = new \App\Controllers\Auth($app);
+  $controller->login();
 });
 
-$router->post('/login', function() {
-  $controller = new ControllerUser();
-  $controller->post_login();
+$router->post('/login', function() use ($app) {
+  $controller = new \App\Controllers\Auth($app);
+  $controller->authenticate();
 });
 
-$router->get('/logout', function() {
-  $controller = new ControllerUser();
-  $controller->get_logout();
+$router->get('/logout', function() use ($app) {
+  $controller = new \App\Controllers\Auth($app);
+  $controller->logout();
 });
 
-$router->get('/img/original/(.*)', function($path) {
-  $controller = new HelperThumbnail($path);
+$router->get('/thumbnail/(\d+)x(\d+)/(.*)', function($width, $height, $path) use ($app) {
+  $controller = new \App\Controllers\Thumbnail($app, $width, $height, $path);
   $controller->serve();
 });
 
-$router->get('/img/(\d+)x(\d+)/(.*)', function($width, $height, $path) {
-  $controller = new HelperThumbnail($path);
-  $controller->setThumbnailSize($width, $height);
-  $controller->serve();
-});
-
-$router->get('/(.*)\.(.*)$', function($path, $extension) {
-  $controller = new ControllerPicturo();
+$router->get('/(.*)\.(.*)$', function($path, $extension) use ($app) {
+  $controller = new \App\Controllers\Picturo($app);
   $controller->displayPicture($path, $extension);
 });
 
-$router->get('/(.*)/page([0-9]*)', function($path, $page = 1) {
-  $controller = new ControllerPicturo();
+$router->get('/(.*)/page([0-9]*)', function($path, $page = 1) use ($app) {
+  $controller = new \App\Controllers\Picturo($app);
   $controller->displayFolder($path, $page);
 });
 
-$router->get('/(.*)', function($path) {
-  $controller = new ControllerPicturo();
+$router->get('/(.*)', function($path) use ($app) {
+  $controller = new \App\Controllers\Picturo($app);
   $controller->displayFolder($path, 1);
 });
 
 $router->run();
-
